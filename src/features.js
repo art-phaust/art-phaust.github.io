52b73607f2059c41a748e7b2de6c17c4771ff7be
Olly Freeman <ollyfreeman209@gmail.com>
2022-03-05 17:14:14 +0000

    Update features with Will's suggestions

--- src/features.js
+++ src/features.js
@@ -15,18 +15,18 @@ function getBackground(config) {
     : 'Duo';
 }
 
-function getLandforms(config) {
+function getProminence(config) {
   const { yLengthBase } = config.user.layers
-  if (yLengthBase <= 0.2) return 'Plains';
-  if (yLengthBase <= 0.4) return 'Hills';
-  return 'Mountains';
+  if (yLengthBase <= 0.2) return 'Shallow';
+  if (yLengthBase <= 0.4) return 'Gradual';
+  return 'Sheer';
 }
 
 function getSun(config) {
   const { isOn, halo } = config.user.sun;
   if (!isOn) return 'None';
-  if (halo.count === 0) return 'Fading';
-  if (halo.count <= 1) return 'Watery';
+  if (halo.count === 0) return 'Mellow';
+  if (halo.count <= 1) return 'Hazy';
   if (halo.count <= 3) return 'Glowing';
   return 'Scorching';
 }
@@ -34,12 +34,12 @@ function getSun(config) {
 function getRain(config) {
   const { isOn, angle } = config.user.rain;
   if (!isOn) return 'None';
-  if (angle <= -70) return 'Torrential';
+  if (angle <= -70) return 'Howling';
   if (angle >= 60) return 'Lashing';
-  if (angle <= -50) return 'Deluge';
+  if (angle <= -50) return 'Pounding';
   if (angle >= 40) return 'Pouring';
-  if (angle <= -30) return 'Moderate';
-  if (angle >= 20) return 'Drizzle';
+  if (angle <= -30) return 'Steady';
+  if (angle >= 20) return 'Light';
   return 'Gentle';
 }
 
@@ -50,18 +50,18 @@ function getBreaks(config) {
 function getDensity(config) {
   const { xDensity: density } = config.user.layers.defaults.strips;
   if (density <= 600) return 'Sparse';
-  if (density <= 900) return 'Moderate';
-  if (density <= 1200) return 'Packed';
-  return 'Dense';
+  if (density <= 900) return 'Light';
+  if (density <= 1200) return 'Crowded';
+  return 'Heavy';
 }
 
 function getDistortion(config) {
   const { xDistortionRange: distortion } = config.user.layers.defaults.strips;
-  if (distortion <= 0.3) return 'Organized';
-  if (distortion <= 0.6) return 'Light';
-  if (distortion <= 0.9) return 'Moderate';
-  if (distortion <= 1.2) return 'Fuzzy';
-  return 'Strong';
+  if (distortion <= 0.3) return 'Sharp';
+  if (distortion <= 0.6) return 'Tidy';
+  if (distortion <= 0.9) return 'Vague';
+  if (distortion <= 1.2) return 'Woozy';
+  return 'Fuzzy';
 }
 
 export function getFeatures(config) {
@@ -69,7 +69,7 @@ export function getFeatures(config) {
     'Palette': getPalette(config),
     'Hue Variation': getHueVariation(config),
     'Background': getBackground(config),
-    'Landforms': getLandforms(config),
+    'Prominence': getProminence(config),
     'Sun': getSun(config),
     'Rain': getRain(config),
     // Name ?
